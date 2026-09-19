5d59b39e242baf8e1130d7f7c342f1eb875cae2e
Ömer Faruk Yıldız <omerfarukyildiz2004@outlook.com>
2026-09-19 14:08:57 +0300

    feat(ui): implement and finalize main Form2 interface

--- Form2.Designer.cs
+++ Form2.Designer.cs
@@ -28,21 +28,127 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.productNameText = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.productCategoryComboBox = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.stockNumericUpDown = new System.Windows.Forms.NumericUpDown();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.priceMaskedTextBox = new System.Windows.Forms.MaskedTextBox();
+            this.saveBtn = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.stockNumericUpDown)).BeginInit();
             this.SuspendLayout();
+            // 
+            // productNameText
+            // 
+            this.productNameText.Location = new System.Drawing.Point(391, 161);
+            this.productNameText.Name = "productNameText";
+            this.productNameText.Size = new System.Drawing.Size(177, 26);
+            this.productNameText.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(261, 164);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(110, 20);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Product Name";
+            // 
+            // productCategoryComboBox
+            // 
+            this.productCategoryComboBox.FormattingEnabled = true;
+            this.productCategoryComboBox.Location = new System.Drawing.Point(391, 206);
+            this.productCategoryComboBox.Name = "productCategoryComboBox";
+            this.productCategoryComboBox.Size = new System.Drawing.Size(177, 28);
+            this.productCategoryComboBox.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(239, 206);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(132, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Product Category";
+            // 
+            // stockNumericUpDown
+            // 
+            this.stockNumericUpDown.Location = new System.Drawing.Point(391, 257);
+            this.stockNumericUpDown.Name = "stockNumericUpDown";
+            this.stockNumericUpDown.Size = new System.Drawing.Size(177, 26);
+            this.stockNumericUpDown.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(321, 263);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(50, 20);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "Stock";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(321, 313);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(44, 20);
+            this.label4.TabIndex = 1;
+            this.label4.Text = "Price";
+            // 
+            // priceMaskedTextBox
+            // 
+            this.priceMaskedTextBox.Location = new System.Drawing.Point(391, 313);
+            this.priceMaskedTextBox.Mask = "₺999.99";
+            this.priceMaskedTextBox.Name = "priceMaskedTextBox";
+            this.priceMaskedTextBox.Size = new System.Drawing.Size(177, 26);
+            this.priceMaskedTextBox.TabIndex = 5;
+            // 
+            // saveBtn
+            // 
+            this.saveBtn.Location = new System.Drawing.Point(391, 362);
+            this.saveBtn.Name = "saveBtn";
+            this.saveBtn.Size = new System.Drawing.Size(177, 41);
+            this.saveBtn.TabIndex = 6;
+            this.saveBtn.Text = "Save";
+            this.saveBtn.UseVisualStyleBackColor = true;
             // 
             // Form2
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ActiveCaption;
-            this.ClientSize = new System.Drawing.Size(1530, 809);
+            this.ClientSize = new System.Drawing.Size(950, 496);
+            this.Controls.Add(this.saveBtn);
+            this.Controls.Add(this.priceMaskedTextBox);
+            this.Controls.Add(this.stockNumericUpDown);
+            this.Controls.Add(this.productCategoryComboBox);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.productNameText);
             this.Name = "Form2";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form2";
+            ((System.ComponentModel.ISupportInitialize)(this.stockNumericUpDown)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox productNameText;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox productCategoryComboBox;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.NumericUpDown stockNumericUpDown;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.MaskedTextBox priceMaskedTextBox;
+        private System.Windows.Forms.Button saveBtn;
     }
 }
